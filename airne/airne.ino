bd87ece664c637067e985f9d5d17cdb786914461
codaked-Adi <adityasinghal408@outlook.com>
2019-04-15 12:24:01 +0530

    Dust Sensor Code Added

--- airne/airne.ino
+++ airne/airne.ino
@@ -19,16 +19,38 @@ void setup() {
   pinMode(buzzer, OUTPUT);
   pinMode(smokeA0, INPUT);
   Serial.begin(9600);
+  pinMode(ledPower,OUTPUT);
 }
 
 void loop() {
   int analogSensor = analogRead(smokeA0);
   int val=analogRead(0);
+  digitalWrite(ledPower,LOW);
+  delayMicroseconds(samplingTime);
+
+  voMeasured = analogRead(measurePin);
+
+  delayMicroseconds(deltaTime);
+  digitalWrite(ledPower,HIGH);
+  delayMicroseconds(sleepTime);
+  calcVoltage = voMeasured*(5.0/1024);
+  dustDensity = 0.17*calcVoltage-0.1;
+  if ( dustDensity < 0)
+  {
+    dustDensity = 0.00;
+  }
  
   Serial.print("Pin A0: ");
   Serial.println(analogSensor);
   Serial.println(analogInPin);
+  Serial.println("Raw Signal Value (0-1023):");
+  Serial.println(voMeasured);
 
+  Serial.println("Voltage:");
+  Serial.println(calcVoltage);
+
+  Serial.println("Dust Density:");
+  Serial.println(dustDensity);
   // Checks if it has reached the threshold value
   if (val < 50)
   {
